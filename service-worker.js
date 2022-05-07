3466b5caef7b6252af2ee07a601ca71af78302ce
wingram1 <waingram96@gmail.com>
2022-05-07 14:21:42 -0500

    webpack config

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const FILES_TO_CACHE = [
+  "./index.html",
+  "./styles.css",
+  "./index.js",
+  "./idb.js",
+];
+
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
