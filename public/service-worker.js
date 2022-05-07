da67be8cc1e4dce931dc9d01b438718d3a4a8e3b
wingram1 <waingram96@gmail.com>
2022-05-07 16:51:43 -0500

    service workers working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,16 @@
-console.log("Hello from your serviceworker.js");
 const FILES_TO_CACHE = [
-  "./public/index.html",
-  "./public/css/styles.css",
-  "./public/js/index.js",
-  "./public/js/idb.js",
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
 ];
 
 const APP_PREFIX = "BudgetTracker-";
